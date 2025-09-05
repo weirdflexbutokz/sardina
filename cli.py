243c946bfac13c35627ad4aa238a176367656fc8
datadiego <juandiegomariscal@gmail.com>
2025-09-05 22:33:56 +0200

    refactorizamos y metemos en main

--- cli.py
+++ cli.py
@@ -2,9 +2,6 @@ import questionary
 import subprocess
 import os
 
-ruta_imagen = questionary.path("¿Cuál es el path de la imagen que deseas flashear?").ask()
-ruta_imagen = os.path.expanduser(ruta_imagen)
-
 def obtener_usbs():
     resultado = subprocess.run(
         ["lsblk", "-o", "NAME,MODEL,TRAN,SIZE,MOUNTPOINT", "-J"],
@@ -19,10 +16,14 @@ def obtener_usbs():
             dispositivos.append(nombre)
     return dispositivos
 
-usbs = obtener_usbs()
-if not usbs:
-    print("No se detectaron dispositivos USB.")
-else:
+def main():
+    ruta_imagen = questionary.path("¿Cuál es el path de la imagen que deseas flashear?").ask()
+    ruta_imagen = os.path.expanduser(ruta_imagen)
+
+    usbs = obtener_usbs()
+    if not usbs:
+        print("No se detectaron dispositivos USB.")
+        return
     dispositivo = questionary.select(
         "¿En qué dispositivo USB deseas flashear la imagen?",
         choices=usbs
@@ -30,17 +31,21 @@ else:
     print(f"Imagen: {ruta_imagen}")
     print(f"Dispositivo seleccionado: {dispositivo}")
 
-    # Sumario y confirmación
     print("\nResumen de selección:")
     print(f"- Imagen a flashear: {ruta_imagen}")
     print(f"- Dispositivo USB: {dispositivo}")
     proceder = questionary.confirm("¿Deseas proceder con el flasheo?").ask()
     if proceder:
-      print("Procediendo con el flasheo...")
-      path_dispositivo = dispositivo.split(" ")[0]
-      comando = ["sudo", "dd", f"if={ruta_imagen}", f"of={path_dispositivo}", "bs=64M", "status=progress", "conv=fsync"]
-      proceso = subprocess.run(comando)
-      if proceso.returncode == 0:
-        print("Flasheo completado exitosamente.")
-      else:
-        print("Ocurrió un error durante el flasheo.")
+        print("Procediendo con el flasheo...")
+        path_dispositivo = dispositivo.split(" ")[0]
+        comando = [
+            "sudo", "dd", f"if={ruta_imagen}", f"of={path_dispositivo}", "bs=64M", "status=progress", "conv=fsync"
+        ]
+        proceso = subprocess.run(comando)
+        if proceso.returncode == 0:
+            print("Flasheo completado exitosamente.")
+        else:
+            print("Ocurrió un error durante el flasheo.")
+
+if __name__ == "__main__":
+    main()
